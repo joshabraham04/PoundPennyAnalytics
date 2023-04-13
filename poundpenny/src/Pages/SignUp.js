6c29f8a0e106f1353e52a1396354aa0af9a543a4
marimonl <lauren.marimon@ufl.edu>
2023-04-13 01:28:45 -0400

    Textbox functionality

--- poundpenny/src/Pages/SignUp.js
+++ poundpenny/src/Pages/SignUp.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './SignUp.css';
 import Button from '../Components/Button';
+import Textbox from '../Components/Textbox';
 
 function SignUp() {
+
+  //BUTTON STUFF
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
@@ -23,6 +26,31 @@ function SignUp() {
 
   window.addEventListener('resize', showButton);
 
+  //TEXTBOX STUFF
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+  function handleFNChange(event) {
+    setFirstName(event.target.value);
+  }
+  function handleLNChange(event) {
+    setLastName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // handle login logic here
+  }
+
   return( 
     <div>
     <div className='signup'>
@@ -33,10 +61,30 @@ function SignUp() {
           and you'll get an e-mail to help you get started.
       </p>
   </div>
-  <div className='creds'>
-  {button && <Button buttonStyle='btn--primary'to='/SignUp'>Sign Up</Button>}
+  <form onSubmit ={handleSubmit}>
+
+ 
+    <div className='creds'>
+      <div className='first'>
+        <label > First Name</label>
+        <Textbox type="firstname" value= {firstname} onChange= {handleFNChange}/>
+        </div>
+        <div className='last'>
+        <label> Last Name</label>
+        <Textbox type="lastname" value= {lastname} onChange= {handleLNChange}/>
+        </div>
+        <div className='email'>
+        <label>E-mail address</label>
+        <Textbox type="email" value= {username} onChange= {handleUsernameChange}/>
+        </div>
+        <div className='pass'>
+        <label> Password</label>
+        <Textbox type="password" value= {password} onChange= {handlePasswordChange}/>
+        </div>
+      {button && <Button buttonStyle='btn--primary'to='/SignUp'>Sign Up</Button>}
 
   </div>
+  </form>
   </div>
   
   );
